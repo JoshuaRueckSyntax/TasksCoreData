2dfcab8cc04e00e6b8e2f60589a15d775ebfb0bf
joshur19 <rueck.joshua@gmail.com>
2024-03-20 15:34:27 +0100

    Initial Commit

---
+++ TasksCoreData/TasksCoreDataApp.swift
@@ -0,0 +1,17 @@
+//
+//  TasksCoreDataApp.swift
+//  TasksCoreData
+//
+//  Created by Joshua Rück on 20.03.24.
+//
+
+import SwiftUI
+
+@main
+struct TasksCoreDataApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
